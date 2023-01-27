f6246da8eca66386b12c143e0e4c41f53f2e3c27
Sebastian Plattner <sebastian.plattner@gmail.com>
2023-01-27 09:28:12 +0100

    set correct ingress class

--- terraform/modules/rke2-cluster/argocd.tf
+++ terraform/modules/rke2-cluster/argocd.tf
@@ -35,25 +35,25 @@ resource "helm_release" "argocd" {
 
 # This can only be applied after the argocd installation, as the CRDs are missing and this terraform provider currently can not work with this
 # See https://github.com/hashicorp/terraform-provider-kubernetes/issues/1367
-resource "kubernetes_manifest" "bootstrap" {
-  manifest = {
-    "apiVersion" = "argoproj.io/v1alpha1"
-    "kind"       = "Application"
-    "metadata" = {
-      "name"      = "bootstrap"
-      "namespace" = helm_release.argocd.namespace
-    }
-    "spec" = {
-      "project": "default"
-      "source": {
-        "repoURL": "https://github.com/acend/infrastructure.git"
-        "targetRevision": "HEAD"
-        "path": "deploy/bootstrap"
-      }
-      "destination": {
-        "server": "https://kubernetes.default.svc"
-        "namespace": helm_release.argocd.namespace
-      }
-    }
-  }
-}
+# resource "kubernetes_manifest" "bootstrap" {
+#   manifest = {
+#     "apiVersion" = "argoproj.io/v1alpha1"
+#     "kind"       = "Application"
+#     "metadata" = {
+#       "name"      = "bootstrap"
+#       "namespace" = helm_release.argocd.namespace
+#     }
+#     "spec" = {
+#       "project": "default"
+#       "source": {
+#         "repoURL": "https://github.com/acend/infrastructure.git"
+#         "targetRevision": "HEAD"
+#         "path": "deploy/bootstrap"
+#       }
+#       "destination": {
+#         "server": "https://kubernetes.default.svc"
+#         "namespace": helm_release.argocd.namespace
+#       }
+#     }
+#   }
+# }
