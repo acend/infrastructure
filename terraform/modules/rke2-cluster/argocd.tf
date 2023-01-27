ef061afeb9cfde0d28b05ab49df9f3e7d4039b06
Sebastian Plattner <sebastian.plattner@gmail.com>
2023-01-27 09:50:40 +0100

    fix bootstrap

--- terraform/modules/rke2-cluster/argocd.tf
+++ terraform/modules/rke2-cluster/argocd.tf
@@ -33,27 +33,17 @@ resource "helm_release" "argocd" {
 
 }
 
-# This can only be applied after the argocd installation, as the CRDs are missing and this terraform provider currently can not work with this
-# See https://github.com/hashicorp/terraform-provider-kubernetes/issues/1367
-# resource "kubernetes_manifest" "bootstrap" {
-#   manifest = {
-#     "apiVersion" = "argoproj.io/v1alpha1"
-#     "kind"       = "Application"
-#     "metadata" = {
-#       "name"      = "bootstrap"
-#       "namespace" = helm_release.argocd.namespace
-#     }
-#     "spec" = {
-#       "project": "default"
-#       "source": {
-#         "repoURL": "https://github.com/acend/infrastructure.git"
-#         "targetRevision": "HEAD"
-#         "path": "deploy/bootstrap"
-#       }
-#       "destination": {
-#         "server": "https://kubernetes.default.svc"
-#         "namespace": helm_release.argocd.namespace
-#       }
-#     }
-#   }
-# }
+resource "helm_release" "argocd-bootstrap" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = kubernetes_namespace.argocd.metadata.0.name
+  version    = "0.0.6"
+  
+  values = [
+    templatefile("${path.module}/templates/argocd-bootstrap-values.yaml", {
+        namespace = helm_release.argocd.namespace
+    }),
+  ]
+
+}
