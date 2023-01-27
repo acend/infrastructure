b503d012952dddccff68e6c251d446a72143aa6f
Sebastian Plattner <sebastian.plattner@gmail.com>
2023-01-27 08:49:53 +0100

    change cluster domain for testing

--- terraform/output.tf
+++ terraform/output.tf
@@ -4,10 +4,12 @@ output "kubernetes_api_host" {
 
 output "kubeconfig" {
   value = module.acend-cluster.kubeconfig
+  sensitive = true
 }
 
 output "kubeconfig_raw" {
   value = module.acend-cluster.kubeconfig_raw
+  sensitive = true
 }
 
 output "argocd-admin-secret" {
