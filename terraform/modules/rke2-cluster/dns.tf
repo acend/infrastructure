4aa85a43f63e2ed9efe10940da3f69af67f9a700
Sebastian Plattner <plattner@puzzle.ch>
2023-02-18 19:24:09 +0100

    add dns record for the ingress load balancer

--- terraform/modules/rke2-cluster/dns.tf
+++ terraform/modules/rke2-cluster/dns.tf
@@ -13,23 +13,31 @@ resource "restapi_object" "lb-api-aaaa-record" {
 }
 
 
-# data "kubernetes_service" "ingress-nginx" {
-#   metadata {
-#     name      = "ingress-nginx-controller"
-#     namespace = "ingress-nginx"
-#   }
-# }
+data "kubernetes_service" "ingress-nginx" {
+  metadata {
+    name      = "ingress-nginx-controller"
+    namespace = "ingress-nginx"
+  }
 
-# resource "restapi_object" "ingress-a-record" {
-#   provider = restapi.hosttech_dns
-#   path = "/api/user/v1/zones/${var.hosttech-dns-zone-id}/records"
-#   data = "{\"type\": \"A\",\"name\": \"ingress-${var.clustername}\",\"ipv4\": \"${data.kubernetes_service.ingress-nginx.status.0.load_balancer.0.ingress.0.ip}\",\"ttl\": 3600,\"comment\": \"Ingress Controller LB\"}"
-#   id_attribute = "data/id"
-# }
+  count = var.first_install ? 0 : 1
+}
 
-# resource "restapi_object" "ingress-aaaa-record" {
-#   provider = restapi.hosttech_dns
-#   path = "/api/user/v1/zones/${var.hosttech-dns-zone-id}/records"
-#   data = "{\"type\": \"AAAA\",\"name\": \"ingress-${var.clustername}\",\"ipv6\": \"${data.kubernetes_service.ingress-nginx.status.0.load_balancer.0.ingress.1.ip}\",\"ttl\": 3600,\"comment\": \"Ingress Controller LB\"}"
-#   id_attribute = "data/id"
-# }
+resource "restapi_object" "ingress-a-record" {
+  provider = restapi.hosttech_dns
+  path = "/api/user/v1/zones/${var.hosttech-dns-zone-id}/records"
+  data = "{\"type\": \"A\",\"name\": \"*.k8s-${var.clustername}\",\"ipv4\": \"${data.kubernetes_service.ingress-nginx.0.status.0.load_balancer.0.ingress.0.ip}\",\"ttl\": 3600,\"comment\": \"Ingress Controller LB\"}"
+  id_attribute = "data/id"
+
+  // Only after initial installation, the nginx lb svc is available.
+  count = var.first_install ? 0 : 1
+
+}
+
+resource "restapi_object" "ingress-aaaa-record" {
+  provider = restapi.hosttech_dns
+  path = "/api/user/v1/zones/${var.hosttech-dns-zone-id}/records"
+  data = "{\"type\": \"AAAA\",\"name\": \"*.k8s-${var.clustername}\",\"ipv6\": \"${data.kubernetes_service.ingress-nginx.0.status.0.load_balancer.0.ingress.1.ip}\",\"ttl\": 3600,\"comment\": \"Ingress Controller LB\"}"
+  id_attribute = "data/id"
+
+  count = var.first_install ? 0 : 1
+}
