1405557641b0fd4f367876ff55ab1c5167d5506d
Daniel S <sybnex@gmail.com>
2023-05-15 15:57:37 +0200

    test: tf provider

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,3 +1,39 @@
 terraform {
-  required_version = ">= 1.3.3"
+  required_version = ">= 1.4"
+
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "1.38.2"
+    }
+    ssh = {
+      source  = "loafoe/ssh"
+      version = "2.6.0"
+    }
+    restapi = {
+      source  = "mastercard/restapi"
+      version = "1.18.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.4"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.20.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.9.0"
+    }
+  }
 }
+
