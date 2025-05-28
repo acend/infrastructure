877a6dc3448e76a189bacc2bb0795a57c3a96b6c
Sebastian Plattner <sebastian.plattner@gmail.com>
2025-05-28 14:54:51 +0200

    upgrade rke2 to v1.33.1+rke2r1

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -55,7 +55,7 @@ variable "clustername" {
 
 variable "rke2_version" {
   type        = string
-  default     = "v1.31.3+rke2r1"
+  default     = "v1.33.1+rke2r1"
   description = "Version of rke2 to install"
 }
 
